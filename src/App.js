96b592c3f044a3bb04563146cddeaec9afdf4a39
Aashir Sohail <asohial.bscs16seecs@seecs.edu.pk>
2020-08-20 21:04:42 +0500

    static frontend populated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Dashboard from './components/dashboard';
+import Signup from './components/signup';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <ul className="navbar-nav mr-auto">
+        <li><Link to={'/'} className="nav-link"> Dashboard </Link></li>
+        <li><Link to={'/signup'} className="nav-link">Signup</Link></li>
+      </ul>
+      </nav>
+      <h3 className = "text-center mt-3">Ingenio CryptoStock App</h3>
+      <Switch>
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/signup' component={Signup} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/dashboard.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+//N9SUKXTJE3657J9P
+async componentDidMount(){
+    let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=1&apikey=N9SUKXTJE3657J9P'
+    let response = await fetch(url);
+    let commits = await response.json();
+    console.log(commits['Meta Data']['2. Symbol'])
+    //console.log(commits.symbol)
+
+}
+  render() {
+    return (
+      <>
+        <div className="container mt-3">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Cras justo odio
+              <span className="badge badge-primary badge-pill">14</span>
+              <a href="#" className="btn btn-primary mr-3 pl-3 pr-3">
+                Buy
+              </a>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Dapibus ac facilisis in
+              <span className="badge badge-primary badge-pill">2</span>
+              <a href="#" className="btn btn-primary mr-3 pl-3 pr-3">
+                Buy
+              </a>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              Morbi leo risus
+              <span className="badge badge-primary badge-pill">1</span>
+              <a href="#" className="btn btn-primary mr-3 pl-3 pr-3">
+                Buy
+              </a>
+            </li>
+          </ul>
+
+          <div className="row mt-5 mb-5 justify-content-center">
+            <div className="col-3">
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  className="card-img-top"
+                  src="https://i2.wp.com/airlinkflight.org/wp-content/uploads/2019/02/male-placeholder-image.jpeg?ssl=1"
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">User Name</h5>
+                  <p className="card-text"></p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-primary mr-3 pl-3 pr-3">
+                      Buy
+                    </a>
+                    <a href="#" className="btn btn-secondary pl-3 pr-3">
+                      Sell
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-left ml-5 col-8">
+              <div className="card wid">
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item text-left">Cras justo odio <a href="#" className="btn btn-secondary ml-5 pl-3 pr-3 text-right">Sell</a></li>
+                  <li className="list-group-item text-left">Dapibus ac faci<a href="#" className="btn btn-secondary ml-5  pl-3 pr-3 text-right">Sell</a></li>
+                  <li className="list-group-item text-left">Vestibulum at e<a href="#" className="btn btn-secondary ml-5  pl-3 pr-3 text-right">Sell</a></li>
+                  <li className="list-group-item text-left">Dapibus ac faci<a href="#" className="btn btn-secondary ml-5  pl-3 pr-3 text-right">Sell</a></li>
+                  <li className="list-group-item text-left">Cras justo odio <a href="#" className="btn btn-secondary ml-5 pl-3 pr-3 text-right">Sell</a></li>
+                  <li className="list-group-item text-left">Vestibulum at e<a href="#" className="btn btn-secondary ml-5  pl-3 pr-3 text-right">Sell</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
